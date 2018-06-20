035bb19f6cd26d579a2b5d64570384901cee29ed
JungJungIn <call518@gmail.com>
2018-06-20 11:34:23 +0900

    Initial commit

---
+++ reset-galera.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kubectl delete service/galera statefulset.apps/galera; rm -rf /data/pods/galera-{0,1,2,3,4}/datadir/*
---
+++ reset-mongo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kubectl delete service/mongo statefulset.apps/mongo; rm -rf /data/pods/mongo-{0,1,2,3,4}/datadir/*
