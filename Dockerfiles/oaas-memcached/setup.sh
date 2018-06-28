41554ee54c2f0423d062152baf9d9e53b6ee4f29
JungJungIn <call518@gmail.com>
2018-06-28 16:08:15 +0900

    Added memcached.yaml, and working keystone.yaml...

---
+++ Dockerfiles/oaas-memcached/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#DB_HOST="haproxy"
+#DB_NAME="keystone"
+#DB_USER=$KEYSTONE_DB_USERNAME
+#DB_PASS=$KEYSTONE_DB_PASSWORD
+
+#MYSQL="/usr/bin/mysql -h$DB_HOST -u$DB_USER -p$DB_PASS"
+
+#$MYSQL_CMD $DB_NAME > /dev/null 2>&1 < /dev/null
+#isExistDB=$?
+#if ! $isExistDB; then
+#	$MYSQL -s -e "CREATE DATABASE keystone;"
+#fi
+
+
+
