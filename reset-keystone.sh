36921fd71a952d6188e555f7b6b8b81f84041f66
JungJungIn <call518@gmail.com>
2018-07-02 17:46:47 +0900

    change keystone pod ttpe: deployment -> statefulset

--- reset-keystone.sh
+++ reset-keystone.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-kubectl delete service/keystone deployment.apps/keystone
+#kubectl delete service/keystone deployment.apps/keystone
+kubectl delete service/keystone statefulset.apps/keystone
 kubectl exec -it galera-0 -- mysql -uroot -ppassw0rd -e "drop database keystone; delete from mysql.user where User='keyston';"
 kubectl get all -o wide
