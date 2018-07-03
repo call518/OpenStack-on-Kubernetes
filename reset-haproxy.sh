236970d62fea39e254943683bb8fa3d85d05dace
JungJungIn <call518@gmail.com>
2018-07-03 18:05:07 +0900

    minor update

--- reset-haproxy.sh
+++ reset-haproxy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kubectl delete service/haproxy service/haproxy-stats deployment.apps/haproxy
+kubectl delete service/haproxy-galera service/haproxy-stats deployment.apps/haproxy
 
 kubectl get all -o wide
