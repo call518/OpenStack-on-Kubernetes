1351156824299da758df87b7c757844591486522
JungJungIn <call518@gmail.com>
2018-07-04 12:56:54 +0900

    created keystone statefulset type

--- reset-keystone.sh
+++ reset-keystone.sh
@@ -3,4 +3,5 @@
 #kubectl delete service/keystone deployment.apps/keystone
 kubectl delete service/keystone statefulset.apps/keystone
 kubectl exec -it galera-0 -- mysql -uroot -ppassw0rd -e "drop database keystone; delete from mysql.user where User='keyston';"
+kubectl exec -it galera-0 -- mysql -uroot -ppassw0rd -e "show databases;"
 kubectl get all -o wide
