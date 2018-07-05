f2e0bc2eb8e9e20e091c9ceeab98ce23f9cbe83b
JungJungIn <call518@gmail.com>
2018-07-05 17:56:03 +0900

    minor update

--- reset-glance.sh
+++ reset-glance.sh
@@ -4,4 +4,5 @@
 kubectl delete service/glance statefulset.apps/glance
 kubectl exec -it keystone-0 -- /bin/bash /scripts/remove-glance.sh
 kubectl exec -it galera-0 -- mysql -uroot -ppassw0rd -e "SHOW DATABASES;"
+rm -rf /data/pods/glance-images/datadir/*
 kubectl get all -o wide
