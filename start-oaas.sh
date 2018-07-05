5fc383ce100d3963adee8b4de24c76a6814d34c2
JungJungIn <call518@gmail.com>
2018-07-05 10:01:56 +0900

    update folder/file structure

--- start-oaas.sh
+++ start-oaas.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-bash -c create-all-pv-pvc.sh
+./create-all-pv-pvc.sh
 
-bash -c update-configMap-env-common.sh
-bash -c update-configMap-keystone-setup.sh
+./update-configMap-env-common.sh
+./update-configMap-keystone-setup.sh
 
-bash -c update-secret-mongodb-keyfile.sh
-bash -c update-secret-rabbitmq-erlang-cookie.sh
-bash -c update-secret-keystone-fernet-key-secrets.sh
+./update-secret-mongodb-keyfile.sh
+./update-secret-rabbitmq-erlang-cookie.sh
+./update-secret-keystone-fernet-key-secrets.sh
 
 kubectl create -f galera-etcd.yaml 
 kubectl create -f galera.yaml
