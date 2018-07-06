573418b3160e0ac71502b93e9120cef477e200f6
JungJungIn <call518@gmail.com>
2018-07-06 16:59:36 +0900

    Pod간 의존성 추가(initContainer)

--- src/stop-oaas.sh
+++ src/stop-oaas.sh
@@ -2,11 +2,12 @@
 
 ./reset-galera-etcd.sh
 ./reset-galera.sh
-./reset-haproxy.sh
-./reset-keystone.sh
 ./reset-memcached.sh
 ./reset-mongodb.sh
 ./reset-rabbitmq.sh
+./reset-haproxy.sh
+./reset-keystone.sh
+./reset-glance.sh
 
 kubectl delete configmap env-common
 kubectl delete configmap glance-setup
