7786bb14a89153358ddf90ca07daa02dc1b4c04e
JungJungIn <call518@gmail.com>
2018-07-08 16:01:56 +0900

    update minor

--- src/start-oaas.sh
+++ src/start-oaas.sh
@@ -4,17 +4,17 @@
 
 ./update-configMap-env-common.sh
 ./update-configMap-keystone-setup.sh
-#./update-configMap-glance-setup.sh
+./update-configMap-glance-setup.sh
 
-#./update-secret-mongodb-keyfile.sh
-#./update-secret-rabbitmq-erlang-cookie.sh
-#./update-secret-keystone-fernet-key-secrets.sh
+./update-secret-mongodb-keyfile.sh
+./update-secret-rabbitmq-erlang-cookie.sh
+./update-secret-keystone-fernet-key-secrets.sh
 
 kubectl create -f galera-etcd.yaml 
 kubectl create -f galera.yaml
 kubectl create -f haproxy.yaml
-#kubectl create -f memcached.yaml
-#kubectl create -f mongodb.yaml
-#kubectl create -f rabbitmq.yaml
-#kubectl create -f keystone.yaml
-#kubectl create -f glance.yaml
+kubectl create -f memcached.yaml
+kubectl create -f mongodb.yaml
+kubectl create -f rabbitmq.yaml
+kubectl create -f keystone.yaml
+kubectl create -f glance.yaml
