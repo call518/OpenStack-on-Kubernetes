cf7edfd55d2670199c5af6fe83f844039abde93f
JungJungIn <call518@gmail.com>
2018-07-20 15:56:53 +0900

    - Chanted to Ocata Release

--- src/reset-cinder.sh
+++ src/reset-cinder.sh
@@ -6,5 +6,7 @@ kubectl delete statefulset.apps/cinder
 
 kubectl exec -it keystone-0 -- /bin/bash /scripts/remove-cinder.sh
 kubectl exec -it galera-0 -- mysql -uroot -ppassw0rd -e "SHOW DATABASES;"
+kubectl exec -it nfs-server -- rm -rf /data/pv/cinder-volumes
+kubectl exec -it nfs-server -- mkdir -p /data/pv/cinder-volumes
 
 kubectl get all -o wide
