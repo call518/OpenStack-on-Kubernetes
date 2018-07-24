378db1c081e830c70e540a59fcb9c3fa66e04008
JungJungIn <call518@gmail.com>
2018-07-24 13:36:02 +0900

    update reset-nfs-server.sh

--- src-ocata/reset-galera.sh
+++ src-ocata/reset-galera.sh
@@ -4,4 +4,6 @@ kubectl delete pdb/galera-pdb
 kubectl delete service/galera
 kubectl delete statefulset.apps/galera
 
+kubectl exec -it nfs-server -- rm -rf /data/pv/galera-{0,1,2}/*
+
 kubectl get all -o wide
