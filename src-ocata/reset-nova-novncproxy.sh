1fde182c83cd969b6728b53132ace0b7d45bdfb3
JungJungIn <call518@gmail.com>
2018-07-24 17:38:34 +0900

    - novncproxy files was removed. (dont need now)

--- src-ocata/reset-nova-novncproxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl delete pdb/nova-novncproxy-pdb
-kubectl delete service/nova-novncproxy
-#kubectl delete statefulset.apps/nova-novncproxy
-kubectl delete deployment.apps/nova-novncproxy
-
-kubectl get all -o wide
--- src-ocata/update-configMap-nova-novncproxy-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if (kubectl get configmap | grep -q "nova-novncproxy-setup"); then
-	kubectl delete configmap nova-novncproxy-setup
-fi
-kubectl create -f configMap-nova-novncproxy-setup.yaml
