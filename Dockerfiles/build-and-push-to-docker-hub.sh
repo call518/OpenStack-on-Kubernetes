9ec2cd6e1c2256160abde1adb866332a72622f99
JungJungIn <call518@gmail.com>
2018-07-31 18:17:44 +0900

    add lbaas menu in horizon dashboard. and update/versioning oaas-ocata docker image, last tag is 1.1

--- Dockerfiles/build-and-push-to-docker-hub.sh
+++ Dockerfiles/build-and-push-to-docker-hub.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+	echo "needed Image Tag, retry~~~"
+	exit
+fi
+
 LPATH=`pwd`
-NAME="call518/`basename $LPATH`"
+NAME="call518/`basename $LPATH`:$1"
 
 docker build -t $NAME . && docker push $NAME
