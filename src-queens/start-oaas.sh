1ec5ee947bbb347d83c95eca72f59170fdeb4722
JungJungIn <call518@gmail.com>
2025-09-10 14:18:17 +0900

    Update start/stop scripts

--- src-queens/start-oaas.sh
+++ src-queens/start-oaas.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eou pipefail
+set -eu -o pipefail
 
 kubectl label nodes k8s-worker1 controller=true nfs-server=true
 kubectl label nodes k8s-worker2 controller=true 
--- src-queens/stop-oaas.sh
+++ src-queens/stop-oaas.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eou pipefail
+set -eu -o pipefail
 
 if [ "$1" != "--force" ]; then
 
